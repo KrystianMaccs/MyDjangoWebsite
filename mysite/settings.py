fc5f3ea716dc849e8877c3678276f2fe6cf44a46
Leli254 <49488373+Leli254@users.noreply.github.com>
2021-04-26 10:07:33 +0300

    Update settings.py
    settings for handling static files i.e css,images,javascript

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,9 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'mysite/static/images/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATICFILES_DIRS = [BASE_DIR / "static",]
 
-STATIC_ROOT = (os.path.join(BASE_DIR, 'root'))
-MEDIA_URL = (os.path.join(BASE_DIR, '/images/'))
-STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static/'))
-                    ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR,'media/')
